f27703ce1125d3b577d429738d883738e65478e2
franckkoukougnon <franckkoukougnon@yahoo.fr>
2022-09-25 16:59:38 +0200

    exception

--- premier_prog/main.py
+++ premier_prog/main.py
@@ -6,7 +6,12 @@ nom = input("Quel est ton nom ? ")
 age = input("Quel est votre age ? ")
 
 # str -> int
-age_prochain = int(age) + 1
 
-print("je suis " + nom + " et j'ai " + str(age) + " ans")
-print("l'annéé prochain vous aurez " + str(age_prochain) + " ans")
+
+try:
+    age_prochain = int(age) + 1
+except:
+    print("Erreur, veuillez mettre des chiffres")
+else:
+    print("je suis " + nom + " et j'ai " + str(age) + " ans")
+    print("l'annéé prochain vous aurez " + str(age_prochain) + " ans")
