97b47ce074330dc731e30c94625cc2287b01ed00
franckkoukougnon <franckkoukougnon@yahoo.fr>
2022-09-25 02:54:05 +0200

    modif

--- premier_prog/main.py
+++ premier_prog/main.py
@@ -8,7 +8,5 @@ age = input("Quel est votre age ? ")
 # str -> int
 age_prochain = int(age) + 1
 
-print(type(age))
-print(type(nom))
 print("je suis " + nom + " et j'ai " + str(age) + " ans")
 print("l'an prochain vous aurez " + str(age_prochain) + " ans")
